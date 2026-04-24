b6c9b242efbeadc3950f277510a466cfbbf724e2
mukerr <2721523076@qq.com>
2026-04-25 02:14:41 +0800

    add example results

--- inst/examples/Gene_related.R
+++ inst/examples/Gene_related.R
@@ -1,17 +1,17 @@
 library(KnockoffPipeline)
 
 run_pipeline(
-    outdir = "output",
+    outdir = "inst/examples/output",
     test_type = "Gene_Centric",
-    pheno_file = "input/phenotype.csv",
-    geno_file = "input/demo",
+    pheno_file = "inst/examples/input/phenotype.csv",
+    geno_file = "inst/examples/input/demo",
     phenotype = "Y",
     pheno_id = "IID",
-    covariates = c("age","sex"),
-    user_cores = 8,             
+    covar_cols = c("X1"),
+    user_cores = 10,             
     sliding_window_length = c(1000,5000,10000),
     geno_missing_imputation = "fixed",
-    plink_path = "your_path_to_plink/plink",
+    plink_path = "/dssg/home/acct-mashiyang1991/mashiyang1991/yujie/ukb_data/imputed/plink2",
     M = 5,
     genome_build = "hg19",
     sample_uncorrelated = FALSE,
